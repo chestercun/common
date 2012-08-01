a2c7c5077d6fa25c625806b58c720f1ba7800c30
Chester Cun <ccun@dhcp-10-150-44-178.cisco.com>
2012-08-01 10:36:23 -0400

    tdd initialized

--- spec/common_spec.rb
+++ spec/common_spec.rb
@@ -1,8 +1,9 @@
-require 'rspec'
-require 'common'
+require 'spec_helper'
 
-describe "something" do
-  xit "can fly" do
+describe "string extensions" do
+  describe "palindrome feature" do
+    subject { "racecar" }
+    it { should be_palindrome }
   end
 end
 
---
+++ spec/spec_helper.rb
@@ -0,0 +1,7 @@
+# code coverage
+require 'simplecov'
+SimpleCov.start
+
+# saucy boss
+require 'rspec'
+require 'common'

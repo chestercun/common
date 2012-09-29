1c44b3ba3a6b33f6d71e4815e54708f61d1ab49b
Chester Cun <ccun@jumping-jack-50.dynamic2.rpi.edu>
2012-09-29 17:54:50 -0400

    sum_digits and reverse_csv functions for string core-extensions

--- lib/ext/string.rb
+++ lib/ext/string.rb
@@ -10,4 +10,22 @@ class String
   def edit_distance(str)
     Common.edit_distance(self,str)
   end
+
+  # Sum the digits of a string
+  #
+  # str = "455"
+  # str.digits_sum => 14
+  #
+  def digits_sum
+    self.split('').map {|x| x.to_i}.inject(0) do |res,elt|
+      res += elt
+      res
+    end
+  end
+
+  # palantir career fair question
+  #
+  def reverse_csv
+    self.split(',').reverse.join(',')
+  end
 end
